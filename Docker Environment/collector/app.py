4bae16a17cff7b8168625440185e2723a6656703
anvitha-rao10 <anvitha8181@gmail.com>
2025-12-29 12:32:40 +0530

    Collector API

--- Docker Environment/collector/app.py
+++ Docker Environment/collector/app.py
@@ -1,44 +1,65 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from datetime import datetime
 import uuid
+import threading
+import time
+
 from jenkins_client import fetch_last_build, fetch_console_log
 
 app = FastAPI()
+
 EVENTS = []
+LAST_BUILD_PROCESSED = None
 
-@app.get("/collector/sync/jenkins")
-def sync_jenkins():
-    try:
-        build = fetch_last_build()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
-    if build.get("result") != "FAILURE":
-        return {"status": "no_failure"}
+def jenkins_watcher():
+    global LAST_BUILD_PROCESSED
 
-    try:
-        log = fetch_console_log()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    while True:
+        try:
+            build = fetch_last_build()
+            build_number = build.get("number")
 
-    event = {
-        "event_id": f"evt-{uuid.uuid4()}",
-        "timestamp": datetime.utcnow().isoformat() + "Z",
-        "source": "jenkins",
-        "ci": {
-            "job_name": build.get("fullDisplayName"),
-            "build_id": build.get("number")
-        },
-        "error": {
-            "error_type": "GIT_SSH_FAILURE",
-            "raw_log": log[:1500]
-        }
-    }
+            if (
+                build.get("result") == "FAILURE"
+                and build_number != LAST_BUILD_PROCESSED
+            ):
+                log = fetch_console_log()
 
-    EVENTS.append(event)
-    return {"status": "collected", "event": event}
+                event = {
+                    "event_id": f"evt-{uuid.uuid4()}",
+                    "timestamp": datetime.utcnow().isoformat() + "Z",
+                    "source": "jenkins",
+                    "ci": {
+                        "job_name": build.get("fullDisplayName"),
+                        "build_id": build_number
+                    },
+                    "error": {
+                        "error_type": "GIT_SSH_FAILURE",
+                        "raw_log": log[:1500]
+                    }
+                }
+
+                EVENTS.append(event)
+                LAST_BUILD_PROCESSED = build_number
+
+        except Exception:
+            pass
+
+        time.sleep(10)  # auto-watch every 10s
+
+
+@app.on_event("startup")
+def start_background_watcher():
+    thread = threading.Thread(target=jenkins_watcher, daemon=True)
+    thread.start()
 
 
 @app.get("/collector/events")
-def show_events():
+def view_events():
     return EVENTS
+
+
+@app.get("/health")
+def health():
+    return {"status": "collector_running"}
